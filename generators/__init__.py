5d21cea02e58b46e90523a4141e041c96bf030c5
az13js <1654602334@qq.com>
2026-01-14 11:31:45 +0800

    完善节奏生成器代码

--- generators/__init__.py
+++ generators/__init__.py
@@ -1,1 +1,40 @@
+from .melody import *
+from .harmony import *
+from .rhythm import *
 
+__all__ = [
+    # 从 melody 模块导出的项
+    "MelodyGenerator",
+    "Note",
+    "Scale",
+    "Key",
+    "generate_melody",
+    "create_progression",
+    "get_note_from_degree",
+
+    # 从 harmony 模块导出的项
+    "ChordProgressionGenerator",
+    "Chord",
+    "ChordQuality",
+    "HarmonicRhythm",
+    "generate_chord_progression",
+    "create_chord",
+    "transpose_chord",
+
+    # 从 rhythm 模块导出的项
+    "NoteType",
+    "RhythmPattern",
+    "RhythmStrategy",
+    "RandomRhythmStrategy",
+    "PatternBasedRhythmStrategy",
+    "SwingRhythmStrategy",
+    "GrooveRhythmStrategy",
+    "HybridRhythmStrategy",
+    "RhythmGenerator",
+    "NOTE_DURATION_MAP",
+    "RHYTHM_PATTERNS",
+    "create_rhythm",
+    "create_groove_rhythm",
+    "create_pattern_rhythm",
+    "get_rhythm_stats",
+]
